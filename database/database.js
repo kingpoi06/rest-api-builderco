717dad0e0a3839e9fa62cdeadf2c5f00ba4d5236
kingpoi06 <putratyo90@gmail.com>
2023-12-15 17:10:37 +0700

    update commit

--- database/database.js
+++ database/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST, 
     user: process.env.DB_USERNAME, 
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
+    database: process.env.DB_DBDATABASE,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
