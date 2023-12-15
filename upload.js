5ce760ad9181cc76d08300c0f43270c126bc7568
kingpoi06 <putratyo90@gmail.com>
2023-12-15 20:06:23 +0700

    update commit

---
+++ upload.js
@@ -0,0 +1,38 @@
+require('dotenv').config()
+const cloudinary = require('cloudinary').v2;
+const pLimit = require('p-limit');
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const images = [
+  './images/jasa-hias-halaman.jpg',
+  './images/jasa-pasang-plafon.jpg',
+  './images/jasa-pemansangan-pintu.jpg',
+  './images/jasa-pemasangan-keramik.jpg',
+  './images/jasa-pemasangan-service-ac.jpg',
+  './images/jasa-pembuatan-taman.jpg',
+  './images/jasa-pengecetan-dinding.jpg',
+  './images/jasa-perbaiki-keramik.jpg',
+  './images/jasa-perbaiki-pipa-air.jpg',
+  './images/jasa-renovasi-rumah.jpg'
+];
+
+(async function run() {
+
+  const limit = pLimit(2);
+
+  const imagesToUpload = images.map((image) => {
+    return limit(async () => {
+      const result = await cloudinary.uploader.upload(image);
+      console.log(`Successfully uploaded ${image}`);
+      console.log(`> Result: ${result.secure_url}`);
+      return result;
+    })
+  });
+
+  await Promise.all(imagesToUpload);
+})();
